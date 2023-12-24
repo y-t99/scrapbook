e23d7f657c49c7221e6fc613d19b31ded852e84e
Itou Ng <13060889537@sina.cn>
2023-12-24 21:29:44 +0800

    feat(editor): data changed listener.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,7 +2,13 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![document_change])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn document_change(event: String) {
+    println!("{}", event);
 }
