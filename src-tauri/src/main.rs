9cf9e6921e7061783c942e55a4190be2ef08e76e
Itou Ng <13060889537@sina.cn>
2023-12-28 21:08:03 +0800

    feat(storage engine): read log file.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,11 +2,12 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::fs::File;
+use std::io::Seek;
 use bson::Document;
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![document_change])
+        .invoke_handler(tauri::generate_handler![document_change, history])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
@@ -18,4 +19,17 @@ fn document_change(events: Vec<Document>) {
         event.to_writer(&buffer).expect("Log Append Error.");
         println!("{}", event);
     }
+}
+
+#[tauri::command]
+fn history() -> Vec<Document> {
+    let buffer = File::options().read(true).open("./../log.bson").expect("Log Open Error.");
+    let meta= buffer.metadata().expect("Get Metadata Error.");
+    let len = meta.len();
+    let mut history: Vec<Document> = Vec::new();
+    while (&buffer).stream_position().expect("Get Seek Error.") < len {
+        let document = Document::from_reader(&buffer).expect("Document Read Error.");
+        history.push(document);
+    }
+    history
 }
