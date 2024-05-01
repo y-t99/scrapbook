d27e424f12862210f72affad8f9768b57ef14a19
Itou Ng <13060889537@sina.cn>
2024-05-01 04:21:12 +0000

    feat: integrate react

---
+++ block-suit-integration/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: 'defaults' }],
+                            ['@babel/preset-react', { targets: 'defaults' }]
+                        ]
+                    }
+                }
+            }
+        ],
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            'React': 'react',
+        }),
+    ],
+};
