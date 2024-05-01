7c80ed76e69d578e6497581d672f1420d72347e2
Itou Ng <13060889537@sina.cn>
2024-05-01 09:24:56 +0000

    feat: integrate block suite

---
+++ block-suite-integration/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from 'react-dom/client';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('app')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
